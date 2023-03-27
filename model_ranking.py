74148c257bc3bbb76518c47191adb32b50a5445a
keenan.gebze <keenan.gebze@gmail.com>
2023-03-28 02:58:54 +0700

    improve example

--- model_ranking.py
+++ model_ranking.py
@@ -2,17 +2,17 @@ from catboost import CatBoost, Pool, CatBoostRanker
 
 # Initialize data
 
-x_train = [["a", 1.5, 4, 5, 6],
-              ["a", 4, 5, 6, 7],
-              ["a", 9.5, 18, 1, 8],
-              ["b", 30, 45.5, 50, 59.5],
-              ["b", 33, 50, 65, 90],
-              ["b", 45, 30, 29.3, 77.1],
-              ["c", 100, 120, 150, 190],
-              ["c", 150, 220, 150, 200],]
+x_train = [["cat1", 1.5, 4, 5, 6],
+              ["cat2", 4, 5, 6, 7],
+              ["cat3", 9.5, 18, 1, 8],
+              ["cat3", 30, 45.5, 50, 59.5],
+              ["cat5", 33, 50, 65, 90],
+              ["cat1", 45, 30, 29.3, 77.1],
+              ["cat2", 100, 120, 150, 190],
+              ["cat3", 150, 220, 150, 200]]
 
-queries_train = ["a", "a", "a", "b", "b", "b", "c", "c"]
-y_train = [1, 0, 0, 1, 1, 0, 1, 1]
+queries_train = ["a", "a", "a", "a", "a", "b", "b", "b"]
+y_train = [1, 0, 1, 1, 1, 0, 0, 1]
 
 train_pool = Pool(
     data=x_train,
@@ -24,24 +24,25 @@ train_pool = Pool(
 # Initialize CatBoostRanker
 model = CatBoost(
     {"iterations": 2000, "verbose": False, "loss_function": 'QueryRMSE'}
-                        #   learning_rate=1,
-                        #   cat_features=[0],
-                        #   depth=2,
-                        #   loss_function='QueryRMSE', 
-                        # verbose=False,
-                        )
+)
+
 # Fit model
 model.fit(train_pool)
 
-eval_data = [
-             ["a", 1.5, 4, 5, 6],
-             ["b", 9.5, 18, 1, 8],
-             ["z", 33, 50, 65, 90],
-             ]
+eval = [
+             ["cat1", 1.5, 4, 5, 6],
+             ["cat2", 9.5, 18, 1, 8],
+             ["cat3", 33, 50, 65, 90],
+        ]
 
+eval_pool = Pool(
+    data = eval,
+    group_id = ["a", "a", "a"],
+    cat_features = [0]
+)
 
-# # Get predictions
-preds = model.predict(eval_data, prediction_type="Probability")
+# # # Get predictions
+preds = model.predict(eval_pool, prediction_type="Probability")
 print(preds)
 
 
